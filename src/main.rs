09848c3c26f1ecb86f1228ac50760e9fdfc8509e
Paul Modlin <paulmodlin@vivaldi.net>
2018-04-20 01:40:08 -0500

    Building up to barebones program

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,33 @@ use domain_coloring::{*,LightnessAlg::*};
 use std::env::args;
 use mylib::*;
 
+//TODO: return Box<Iterator> or use struct w/ impl Iterator?
+fn parse_as_c64(slice: &[String]) -> Vec<Complex64> {
+    let mut v: Vec<Complex64> = vec![];
+    for s in slice {
+        if let Ok(MyC64(num)) = s.parse::<MyC64>() {
+            f.push(num);
+        } else {
+            eprintln!("Warning: could not parse {} as a Complex number, ignoring", s);
+        }
+    }
+    v
+}
+
+fn parse_as_roots(slice &[String]) -> &ComplexFunction {
+    C64Roots(parse_as_c64(slice))
+}
+
+fn parse_as_terms(slice &[String]) -> &ComplexFunction {
+    C64Terms(parse_as_c64(slice))
+}
 
 fn main() {
-    //let mut f: Vec<Complex64>;
+    let mut argv: Vec<String> = vec![];
     for arg in args() {
-        println!("{}", arg);
-        println!("{:?}", arg.parse::<MyC64>());
+        argv.push(arg);
     }
+    for num in parse_as_c64(&argv[1..]) {
+        println!("Parsed: {}", num)
+    };
 }
